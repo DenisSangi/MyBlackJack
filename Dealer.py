36ab7937ca52a9e8fd17aab1a4c593398ac97f1d
DenisSangi <dehc@mail.ru>
2023-02-11 19:25:45 +0200

    Added template of Player and Dealer classes

---
+++ Dealer.py
@@ -0,0 +1,10 @@
+class Dealer:
+
+    dealer_name = "Dealer"
+
+    def __init__(self):
+        self.dealer_name = self.dealer_name
+
+    def deal_a_card(self):
+
+    def sum_of_cards(self):
---
+++ Player.py
@@ -0,0 +1,23 @@
+from Cards import Cards
+
+
+class Player:
+
+    player_name = ""
+    wins_score = 0
+    loses_score = 0
+
+    def __init__(self):
+        self.player_name
+
+    def take_a_card(self):
+
+    def end_turn(self):
+
+    def win_a_game(self):
+        self.wins_score + 1
+
+    def lose_a_game(self):
+        self.loses_score + 1
+
+
