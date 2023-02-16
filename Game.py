8ee4927dea89ec11944c4735ab85f3a2b5b44041
DenisSangi <dehc@mail.ru>
2023-02-16 21:23:37 +0200

    card_score() modified

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ class Game:
     @staticmethod
     def start_game():
 
+        player = Player("")
         dealer = Player("Dealer")
         Player.wins_count = 0
         Player.loses_count = 0
@@ -17,46 +18,46 @@ class Game:
             exit()
 
         elif choice_1 == "y":
-            player = Player("")
-            player.name = Player(input("Enter your name: "))
+
+            player.name = input("Enter your name: ")
             player.get_card()
             player.get_card()
-            Game.cards_score(player)
+            Player.cards_score(player)
 
-            if Game.cards_score(player) == 21:
-                print("You have a BlackJack congratulations!")
-                Player.wins_count += 1
-                Game.end_game()
+        if Player.cards_score(player) == 21:
+            print("You have a BlackJack congratulations!")
+            Player.wins_count += 1
+            Game.end_game()
 
-            elif Game.cards_score(player) < 21:
-                choice_2 = input("Would you like to get another card? "
+        elif Player.cards_score(player) < 21:
+            choice_2 = input("Would you like to get another card? "
                                  "Print 'y' or 'n': ")
-                while choice_2 == 'y' and Game.cards_score(player) < 21:
-                    player.get_card()
-                    Game.cards_score(player)
+            while choice_2 == 'y' and Player.cards_score(player) < 21:
+                player.get_card()
+                Player.cards_score(player)
 
-            if Game.cards_score(player) > 21:
+        if Player.cards_score(player) > 21:
+            Player.loses_count += 1
+            print("You lose")
+            Game.end_game()
+        else:
+            dealer.get_card()
+            dealer.get_card()
+            Player.cards_score(dealer)
+
+            if Player.cards_score(dealer) == 21:
+                print("Dealer is having BlackJack!")
                 Player.loses_count += 1
-                print("You lose")
                 Game.end_game()
-            else:
-                dealer.get_card()
-                dealer.get_card()
-                Game.cards_score(dealer)
 
-                if Game.cards_score(dealer) == 21:
-                    print("Dealer is having BlackJack!")
-                    Player.loses_count += 1
+            while Player.cards_score(dealer) < 17:
+                dealer.get_card()
+                Player.cards_score(dealer)
+
+                if Player.cards_score(dealer) > 21:
+                    Player.wins_count += 1
+                    print("Yow win.")
                     Game.end_game()
-
-                while Game.cards_score(dealer) < 17:
-                    dealer.get_card()
-                    Game.cards_score(dealer)
-
-                    if Game.cards_score(dealer) > 21:
-                        Player.wins_count += 1
-                        print("Yow win.")
-                        Game.end_game()
 
     @staticmethod
     def end_game():
@@ -68,19 +69,4 @@ class Game:
             print("Bye bye")
             exit()
 
-    @staticmethod
-    def cards_score(player):
-        sum_of_cards = 0
-        sum_of_cards_with_ace = 0
 
-        for card in player.player_cards:
-            if card.card_value == "Ace":
-                sum_of_cards_with_ace += card.card_points
-                if sum_of_cards_with_ace < 21:
-                    sum_of_cards = sum_of_cards_with_ace
-                else:
-                    card.card_points = 1
-                    sum_of_cards += card.card_points
-
-        print("{name} is now having {score}.".format(name=player.name, score=sum_of_cards))
-        return sum_of_cards
--- Player.py
+++ Player.py
@@ -1,8 +1,9 @@
 from Cards import Cards
 
+
 class Player:
     name = ""
-    player_cards = [Cards()]
+    player_cards = []
     wins_count = 0
     loses_count = 0
 
@@ -12,4 +13,23 @@ class Player:
     def get_card(self):
         generated_card = Cards()
         Player.player_cards.append(generated_card)
-        print("{name} is have {value} of {suit}.".format(name=self.name, value=generated_card.card_value, suit=generated_card.card_suit))
+        print("{name} is have {value} of {suit}.".format(name=self.name, value=generated_card.card_value,
+                                                         suit=generated_card.card_suit))
+
+    @staticmethod
+    def cards_score(player):
+        sum_of_cards = 0
+        sum_of_cards_with_ace = 0
+
+        for card in player.player_cards:
+            # if card.card_value == "Ace":
+            #     sum_of_cards_with_ace += card.card_points
+            #
+            # if sum_of_cards_with_ace < 21:
+            #     sum_of_cards = sum_of_cards_with_ace
+            # else:
+            #     card.card_points = 1
+            sum_of_cards += card.card_points
+        print("{name} is now having {score}.".format(name=player.name, score=sum_of_cards))
+        return sum_of_cards
+
