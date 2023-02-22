b1d9819457c26110ade3f744635dbd823b3cf48f
DenisSangi <dehc@mail.ru>
2023-02-22 18:29:08 +0200

    Trying resolve Ace calculation

--- Dealer.py
+++ Dealer.py
@@ -19,16 +19,22 @@ class Dealer:
 
     @staticmethod
     def cards_score(dealer):
-        sum_of_cards = 0
+        sum_of_dealer_cards = 0
         sum_with_ace = 0
+
         for card in dealer.dealer_cards:
+            ace_counter = 0
             if card.card_value == "Ace" in dealer.dealer_cards:
+                ace_counter += 1
                 sum_with_ace += card.card_points
-                sum_of_cards = sum_with_ace
-                if sum_with_ace > 21:
-                    card.card_points = 1
-                    sum_with_ace += card.card_points
-                    sum_of_cards = sum_with_ace
+                if ace_counter == 1 and sum_with_ace > 21:
+                    sum_with_ace += card.card_points - ace_counter * 10
+                    sum_of_dealer_cards = sum_with_ace
+                elif ace_counter > 1:
+                    while sum_with_ace > 21:
+                        sum_with_ace += card.card_points - (ace_counter-1) * 10
+                        sum_of_dealer_cards = sum_with_ace
+                        break
             else:
-                sum_of_cards += card.card_points
-            return sum_of_cards
+                sum_of_dealer_cards += card.card_points
+        return sum_of_dealer_cards
--- Player.py
+++ Player.py
@@ -19,16 +19,22 @@ class Player:
 
     @staticmethod
     def cards_score(player):
-        sum_of_cards = 0
+        sum_of_player_cards = 0
         sum_with_ace = 0
+
         for card in player.player_cards:
+            ace_counter = 0
             if card.card_value == "Ace" in player.player_cards:
+                ace_counter += 1
                 sum_with_ace += card.card_points
-                sum_of_cards = sum_with_ace
-                if sum_with_ace > 21:
-                    card.card_points = 1
-                    sum_with_ace += card.card_points
-                    sum_of_cards = sum_with_ace
+                if ace_counter == 1 and sum_with_ace > 21:
+                    sum_with_ace += card.card_points - ace_counter * 10
+                    sum_of_player_cards = sum_with_ace
+                elif ace_counter > 1:
+                    while sum_with_ace > 21:
+                        sum_with_ace += card.card_points - (ace_counter-1) * 10
+                        sum_of_player_cards = sum_with_ace
+                        break
             else:
-                sum_of_cards += card.card_points
-        return sum_of_cards
+                sum_of_player_cards += card.card_points
+        return sum_of_player_cards
